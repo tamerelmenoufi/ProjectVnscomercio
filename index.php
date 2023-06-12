194ce5905b834a5eef78aaf10665aac9f56e86c5
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-12 15:55:21 +0300

    Instalação

--- index.php
+++ index.php
@@ -1,20 +1,76 @@
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>VNS Comércio</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  </head>
-  <body>
+    <title>Project</title>
+    <link href="https://project.tec.br/lib/vendor/bootstrap-5.2.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://project.tec.br/lib/vendor/jquery-confirm-v3.3.4/dist/jquery-confirm.min.css" rel="stylesheet" >
 
-    <div class="row g-0">
-        <div class="container">
-            <div class="text-bg-light p-3">Light with contrasting color</div>
-        </div>
+
+<script src="https://project.tec.br/lib/vendor/jquery-3.6.0/jquery-3.6.0.min.js" ></script>
+<script src="https://project.tec.br/lib/vendor/jquery-confirm-v3.3.4/dist/jquery-confirm.min.js" ></script>
+
+<link href="https://project.tec.br/lib/vendor/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
+
+<script src="https://project.tec.br/lib/vendor/jQuery-Mask/jquery.mask.min.js" ></script>
+
+<script src="https://project.tec.br/lib/vendor/chart/chart.min.js" ></script>
+
+<link href="https://project.tec.br/lib/css/app.css" rel="stylesheet">
+<script src="https://project.tec.br/lib/js/app.js" ></script>
+
+
+<!-- JQUERY UI -->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>  </head>
+    <style>
+        body {
+
+            background:#fff;
+
+        }
+    </style>
+  <body>
+    <div class="Carregando">
+        <div><i class="fa-solid fa-rotate fa-pulse"></i></div>
     </div>
 
-    <h1>Hello, world!</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6" style="text-align:center">
+                    <img src="img/logo.jpg" class="img-fluid">
+                </div>
+                <div class="col-md-6" style="text-align:center">
+                    <h2 style="color:#16999a">EM DESENVOLVIMENTO</h2>
+                </div>
+            </div>
+        </div>
+
+    <script src="https://project.tec.br/lib/vendor/bootstrap-5.2.0/js/bootstrap.bundle.min.js"></script>
+
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSnblPMOwEdteX5UPYXf7XUtJYcbypx6w&language=pt&region=BR"
+    async
+></script>
+
+
+<script src="https://cdn.ckeditor.com/ckeditor5/35.3.0/classic/ckeditor.js"></script>
+    <script>
+        $(function(){
+
+        })
+
+        //Jconfirm
+        jconfirm.defaults = {
+            typeAnimated: true,
+            type: "blue",
+            smoothContent: true,
+        }
+
+    </script>
+
   </body>
 </html>
